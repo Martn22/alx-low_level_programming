6cbfe653696070658010539dc0bb76423a816f58
Martin Kiilu <martinkiilu001@gmail.com>
2022-09-12 07:46:07 -0700

    combinaton sng dgt num

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - This function prints '0, 1, 2, 3, 4, 5, 6, 7, 8, 9'
+ * in ascending order.
+ *
+ * Description: The main function prints single digit numbers
+ * in ascending order.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 48 ; c <= 57 ; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
