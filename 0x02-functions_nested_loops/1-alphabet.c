76b0380a2b68f3a35d30443848eb6665b4b9c7b5
Martin Kiilu <martinkiilu001@gmail.com>
2022-09-13 12:58:25 -0700

    1 task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ * Return: 0 always
+ *
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		_putchar('ch');
+	}
+	_putchar('\n');
+	return (0);
+}
