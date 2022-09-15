ae2336b80c0d4c34ff55fa580992368513410a70
Martin Kiilu <martinkiilu001@gmail.com>
2022-09-15 10:54:29 -0700

    5 task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,7 +8,7 @@
 
 void more_numbers(void)
 {
-	int x, y:
+	int x, y;
 
 	for (x = 0; x < 10; x++)
 	{
@@ -18,8 +18,7 @@ void more_numbers(void)
 			{
 				_putchar((y / 10) + '0');
 			}
-			
-			_putchar((y % 10) + '0');
+				_putchar((y % 10) + '0');
 		}
 		_putchar('\n');
 	}
