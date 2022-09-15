38f2d1d15d0612276b7c4328bb4b98e2c06eec5a
Martin Kiilu <martinkiilu001@gmail.com>
2022-09-15 07:40:32 -0700

    3 task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 
 /**
  * print_numbers - prints 0 to 9
- *
+ * Return: 0
  */
 
 void print_numbers(void)
 {
 	int a;
 
-	for (a = 0 ; a <= 9 ; a++)
+	for (a = '0' ; a <= '9' ; a++)
 	{
 		_putchar(a);
 	}
