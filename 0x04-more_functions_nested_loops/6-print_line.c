6618e4ad45158f6e2d79a93d70046afbab7ee15d
Martin Kiilu <martinkiilu001@gmail.com>
2022-09-15 11:04:31 -0700

    6 task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - draws line according to parameter set
+ * @n: The number of lines to draw
+ *
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+
+		_putchar('\n');
+	}
+}
