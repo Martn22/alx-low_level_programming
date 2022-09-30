6e11b1da9820530af0b5c680cc09a4b0a843ba2a
Martin Kiilu <martinkiilu001@gmail.com>
2022-09-30 11:57:29 -0700

    0 task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - This function prints its program name followed
+ * by a new line.
+ * @argc: The count of the arguments suppplied to the program
+ * @argv: array of pointers to string of the arguments
+ *
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is the 0x0A.c arg c, argv tasks directory
