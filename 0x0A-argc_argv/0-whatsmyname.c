406bbe1c6624fc965a0458bdf2f75f1dc8b05199
Martin Kiilu <martinkiilu001@gmail.com>
2022-09-30 12:09:17 -0700

    0 task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,11 +6,13 @@
  * @argc: The count of the arguments suppplied to the program
  * @argv: array of pointers to string of the arguments
  *
- * Return: returns 0
+ * Return: returns 0 (success)
  */
 
 int main(int argc, char *argv[])
 {
+	(void) argc;
+
 	printf("%s\n", argv[0]);
 
 	return (0);
