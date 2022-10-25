d3a774895f682193a1eb1b561c17283e6d4b5e8b
Martin Kiilu <martinkiilu001@gmail.com>
2022-10-25 02:05:58 -0700

    header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,4 +28,4 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 
-#endif /*LIST_H*/
+#endif
